16eb560cacf6f2966d475fb7f6ca3fc2fedfad8a
Joshua Koo <zz85nus@gmail.com>
2016-11-01 14:33:06 +0800

    Rename variables

--- ansi.js
+++ ansi.js
@@ -1,9 +1,10 @@
 // Adopted from blessed/vendor/tng.js
 
-const ascii_only = !true;
 const colors = require('blessed/lib/colors');
-const ascii = true;
-const no_bg = true;
+
+let plain_formatting = !true;
+let ascii_formatting = true;
+let bg_formatting = true;
 
 // 3 Modules
 // ANSI only - No colors
@@ -11,97 +12,99 @@ const no_bg = true;
 // ASCII + BG COLORS + FG Colors
 // ASCII + FG Colors
 
-module.exports = (function() {
-    this.colors = colors;
+let dchars;
+let bga = 1.0, fga = 0.9;
 
-    let dchars;
-    // Taken from libcaca:
-    dchars = '????8@8@#8@8##8#MKXWwz$&%x><\\/xo;+=|^-:i\'.`,  `.        '.split('').reverse().join('')
-    // dchars = ' .,:;=|iI+hHOE#`$'
+// Taken from libcaca:
+dchars = '????8@8@#8@8##8#MKXWwz$&%x><\\/xo;+=|^-:i\'.`,  `.        '.split('').reverse().join('')
 
-    // darker bolder character set from https://github.com/saw/Canvas-ASCII-Art/
-    // dchars = ' .\'`^",:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
-    // dchars = ' .:-=+*#%@';
+// dchars = ' .,:;=|iI+hHOE#`$'
 
+// darker bolder character set from https://github.com/saw/Canvas-ASCII-Art/
+// dchars = ' .\'`^",:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+// dchars = ' .:-=+*#%@';
 
-    dchars = dchars.split('').reverse().join('');
+dchars = dchars.split('').reverse().join('');
 
-    function pixelToSGR(pixel, ch) {
-        var bga = 1.0
-            , fga = 0.5
-            , a = pixel.a / 255
-            , bg
-            , fg;
+function pixelToSGR(pixel, ch) {
+    var a = pixel.a / 255
+        , bg
+        , fg;
 
-        bg = this.colors.match(
-            pixel.r * a * bga | 0,
-            pixel.g * a * bga | 0,
-            pixel.b * a * bga | 0);
+    bg = colors.match(
+        pixel.r * a * bga | 0,
+        pixel.g * a * bga | 0,
+        pixel.b * a * bga | 0);
 
-        if (ch && ascii) {
-            fg = this.colors.match(
-            pixel.r * a * fga | 0,
-            pixel.g * a * fga | 0,
-            pixel.b * a * fga | 0);
-            if (a === 0 || no_bg) {
+    if (ch && !bg_formatting) {
+        fg = colors.match(
+        pixel.r * a * fga | 0,
+        pixel.g * a * fga | 0,
+        pixel.b * a * fga | 0);
+        if (a === 0 || no_bg) {
             return '\x1b[38;5;' + fg + 'm' + ch + '\x1b[m';
-            }
-            return '\x1b[38;5;' + fg + 'm\x1b[48;5;' + bg + 'm' + ch + '\x1b[m';
         }
-
-        if (a === 0) return ' ';
-
-        return '\x1b[48;5;' + bg + 'm \x1b[m';
+        return '\x1b[38;5;' + fg + 'm\x1b[48;5;' + bg + 'm' + ch + '\x1b[m';
     }
 
-    function luminance(pixel) {
-        var a = pixel.a / 255
-            , r = pixel.r * a
-            , g = pixel.g * a
-            , b = pixel.b * a
-            , l = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    if (a === 0) return ' ';
 
-        return l / 255;
-    };
+    return '\x1b[48;5;' + bg + 'm \x1b[m';
+}
 
-    function getOutch(pixel) {
-        var lumi = luminance(pixel)
-        , outch = dchars[lumi * (dchars.length - 1) | 0];
+function luminance(pixel) {
+    var a = pixel.a / 255
+        , r = pixel.r * a
+        , g = pixel.g * a
+        , b = pixel.b * a
+        , l = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
-        return outch;
-    }
+    return l / 255;
+};
 
-    function convert(image, targetWidth, targetHeight) {
-        const { width, height, data } = image;
-        let out = [];
-        let ty, tx, i, pixel, outch;
-        for (let y = 0; y < targetHeight; y++) {
-            ty = y / targetHeight * height | 0;
-            for (let x = 0; x < targetWidth; x++) {
-                tx = x / targetWidth * width | 0;
-                i = ty * width + tx;
+function getOutch(pixel) {
+    var lumi = luminance(pixel)
+    , outch = dchars[lumi * (dchars.length - 1) | 0];
 
-                pixel = {
-                    r: data[i * 4 + 0],
-                    g: data[i * 4 + 1],
-                    b: data[i * 4 + 2],
-                    a: data[i * 4 + 0]
-                }
+    return outch;
+}
 
-                outch = getOutch(pixel);
-                if (ascii_only) {
-                    out.push(outch);
-                }
-                else {
-                    out.push(pixelToSGR(pixel, outch));
-                }
+function convert(image, targetWidth, targetHeight) {
+    const { width, height, data } = image;
+    let out = [];
+    let ty, tx, i, pixel, outch;
+    for (let y = 0; y < targetHeight; y++) {
+        ty = y / targetHeight * height | 0;
+        for (let x = 0; x < targetWidth; x++) {
+            tx = x / targetWidth * width | 0;
+            i = ty * width + tx;
+
+            pixel = {
+                r: data[i * 4 + 0],
+                g: data[i * 4 + 1],
+                b: data[i * 4 + 2],
+                a: data[i * 4 + 0]
+            };
+
+            outch = getOutch(pixel);
+            if (plain_formatting) {
+                out.push(outch);
             }
-            out.push('\n');
+            else {
+                out.push(pixelToSGR(pixel, outch));
+            }
         }
-        return out.join('');
+        out.push('\n');
     }
+    return out.join('');
+}
 
-    return {
-        convert: convert
-    }
-})();
+module.exports = {
+    convert: convert,
+    setFormatting(f) {
+        plain_formatting = f
+    },
+    setPalatte(p) {
+        dchars = p;
+    },
+};
